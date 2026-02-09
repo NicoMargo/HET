da7c27182fb65d75eead4a32656e0d8e2419ba96
Akhmat01dev <agadzhiev01@gmail.com>
2026-02-09 19:38:45 +0100

    añadir ficheros 1 y 1.3

---
+++ Practica 1/Práctica-1.3.py
@@ -0,0 +1,25 @@
+import socket
+
+host = "www.upv.es"
+port = 80
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+peticion = "GET / HTTP/1.1\r\nHost: www.upv.es\r\n\r\n"
+s.send(peticion.encode())
+
+
+response = b""  # inicializar bytes
+try:
+    while True:
+        data = s.recv(4096)
+        if not data:
+            break
+        response += data
+except socket.timeout:
+    print("Timeout al recibir la respuesta")
+
+print(response.decode(errors="ignore"))
+
+s.close()
---
+++ Practica 1/Práctica-1.py
@@ -0,0 +1,39 @@
+x = 5
+
+y = 4.5
+
+palabra = "hola\n"
+
+texto = """esto 
+es una frase 
+con salto integrado\n"""
+
+lista_mutable = [1,2,3,4]
+
+lista_mutable[1] = 20
+
+lista_inmutable = (10,20,30,40)
+
+#lista_inmutable[1] = 200
+
+diccionario = {
+    "ciudad" : "Valencia"
+}
+
+conjunto = {1,2,3,3,3,3}
+
+
+
+print(x)
+print(y)
+print(palabra)
+print(texto)
+print(lista_mutable)
+print(lista_inmutable)
+print(diccionario)
+print(conjunto)
+
+conjunto[1] = 40
+
+#print(conjunto)
+
