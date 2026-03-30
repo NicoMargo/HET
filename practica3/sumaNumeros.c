1273db14b892a4837f0528b107d0272b3865fd8f
nicomargo <nmargo@pm.me>
2026-03-30 17:01:38 +0200

    practica 3

--- practica3/sumaNumeros.c
+++ practica3/sumaNumeros.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World\n");
-    
-    int suma(int num1, int num2, int num3){
+int suma(int num1, int num2, int num3){
         
         int resultado = num1 + num2;
         resultado = resultado + num3;
         return resultado;
-    }
-    
-    printf("%d", suma(1,2,3));
+}
 
+int main()
+{
+    int resultado = suma(1,2,3);
+
+    printf("%d", resultado);
     return 0;
 }
